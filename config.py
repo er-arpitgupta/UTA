d5bfe83461a19844ff59ff903442ca0fe026ef00
Arpit Gupta <er.gupta.arpit@gmail.com>
2022-05-24 23:21:02 +0530

    Initial commit

---
+++ config.py
@@ -0,0 +1,6 @@
+from os import environ
+
+class Config(object):
+    SECRET_KEY = environ.get('SECRET_KEY') or b'6\xe9\xda\xead\x81\xf7\x8d\xbbH\x87\xe8m\xdd3%'
+
+    MONGODB_SETTINGS = { 'db' : 'UTA_Enrollment' }
